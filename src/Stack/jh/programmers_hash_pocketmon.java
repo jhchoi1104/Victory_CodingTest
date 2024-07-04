d903a1a19ed87ece6e9a0239e2fff5aab343a48e
jhchoi1104 <163481864+jhchoi1104@users.noreply.github.com>
2024-07-05 00:08:27 +0900

    2주차 해시문제2

--- src/Stack/jh/programmers_hash_pocketmon.java
+++ src/Stack/jh/programmers_hash_pocketmon.java
@@ -14,3 +14,7 @@ class Solution {
         return set.size();
     }
 }
+
+// 배운 내용1: HashSet에서 사용할 수 있는 데이터타입은 Integer, String와 같이 참조형. 따라서 int은 사용불가
+// 배운 내용2: 배열의 길이는 nums.length, 해시셋의 길이는 set.size()
+// 시간복잡도: O(N)
