40dd0787527052f44d2c0def797b96aebc2c4139
owzl <alliwannado1211@gmail.com>
2024-07-15 19:54:10 +0900

    week2/stac_queue_sort/yr

--- src/Stack_Queue_String_2week/yr/programmers_K번째수.java
+++ src/Stack_Queue_String_2week/yr/programmers_K번째수.java
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.Arrays;
 
 class Solution{
     public int[] solution(int[] array, int[][] commands){
@@ -9,10 +9,15 @@ class Solution{
              /*
              '~번째'랑 '인덱스'랑 구분해서 범위를 잘 처리해줘야 함
              문제에서 말하는 건 '~번째', 내가 코드로 짜는 건 '인덱스'
+             => k번째 = 인덱스 [k]-1
              */
             // copyOfRange(): 특정 범위 배열 복사
-            int temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1])
-
+            int temp[] = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            // 복사한 배열 정렬하기
+            Arrays.sort(temp);
+            // k번째 수 구하기
+            answer[i] = temp[commands[i][2]-1];
         }
+        return answer;
     }
 }
